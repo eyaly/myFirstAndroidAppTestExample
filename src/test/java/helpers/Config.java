11caa6b751d146de0323e8baf37160c8f67174e3
Eyal Yovel <eyalyovel@gmail.com>
2020-11-10 11:15:16 +0000

    Changes in the files

---
+++ src/test/java/helpers/Config.java
@@ -0,0 +1,7 @@
+package helpers;
+
+public class Config {
+    public static final String region = System.getProperty("region", "eu");
+    public static final String build = System.getProperty("build", "");
+
+}
--- src/test/java/tests/myFirstAndroidAppTest.java
+++ src/test/java/tests/myFirstAndroidAppTest.java
@@ -15,8 +15,8 @@ import org.testng.annotations.Test;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import static helpers.utils.getProperty;
+import static helpers.Config.region;
+import static helpers.Config.build;
 
 
 public class myFirstAndroidAppTest {
@@ -33,23 +33,18 @@ public class myFirstAndroidAppTest {
     @BeforeMethod
     public void setup(Method method) throws MalformedURLException {
 
-        System.out.println("Sauce - BeforeMethod hook");
-
-        String region = getProperty("region", "eu");
-
         String username = System.getenv("SAUCE_USERNAME");
         String accesskey = System.getenv("SAUCE_ACCESS_KEY");
         String methodName = method.getName();
-
-        System.out.println("Sauce - username: " + username + " .accesskey: " + accesskey + " .methodName: " + methodName);
+        System.out.println("Sauce - BeforeMethod hook. Test name: " + methodName);
 
         String sauceUrl;
-
         if (region.equalsIgnoreCase("eu")) {
             sauceUrl = "@ondemand.eu-central-1.saucelabs.com:443";
         } else {
             sauceUrl = "@ondemand.us-west-1.saucelabs.com:443";
         }
+
 
         String SAUCE_REMOTE_URL = "https://" + username + ":" + accesskey + sauceUrl +"/wd/hub";
         URL url = new URL(SAUCE_REMOTE_URL);
@@ -57,21 +52,17 @@ public class myFirstAndroidAppTest {
         String appName ="myFirstApp_version2.0_debug.apk";
 
         MutableCapabilities capabilities = new MutableCapabilities();
-//        capabilities.setCapability("deviceName", "Android");
-//        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("deviceName", "Samsung.*");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("name", methodName);
         capabilities.setCapability("app", "storage:filename=" + appName);
         capabilities.setCapability("phoneOnly", true);
 
-        String build = getProperty("build", "default");
-        System.out.println("Sauce. Build name: " + build);
-        capabilities.setCapability("build", build);
-
-        System.out.println("Sauce. URL: " + url);
-        // Launch remote browser and set it as the current thread
-     //   androidDriver.set(new AndroidDriver(new URL(SAUCE_REMOTE_URL),capabilities));
+        if (!build.equalsIgnoreCase("")) {
+            System.out.println("Sauce. Build name: " + build);
+            capabilities.setCapability("build", build);
+        }
         androidDriver.set( new AndroidDriver(url, capabilities));
         String id = ((RemoteWebDriver) getAndroidDriver()).getSessionId().toString();
         sessionId.set(id);
@@ -110,26 +101,4 @@ public class myFirstAndroidAppTest {
 
         Assert.assertEquals(expectedResult,actualRes );
     }
-
-//    @Test
-//    public void calcAddNumNegative() {
-//        String strNum1 = "2";
-//        String strNum2 = "7";
-//        String expectedResult = "9";
-//
-//        System.out.println("Sauce - Start calcAddNumNegative test");
-//        WebElement num1Edit = (WebElement) androidDriver.get().findElementById(num1);
-//        num1Edit.sendKeys(strNum1);
-//
-//        WebElement num2Edit = (WebElement) androidDriver.get().findElementById(num2);
-//        num2Edit.sendKeys(strNum2);
-//
-//        WebElement sumButtonElement = (WebElement) androidDriver.get().findElementById(sumButton);
-//        sumButtonElement.click();
-//
-//        WebElement resultEdit = (WebElement) androidDriver.get().findElementById(result);
-//        String actualRes = resultEdit.getText();
-//
-//        Assert.assertEquals(expectedResult,actualRes );
-//    }
 }
